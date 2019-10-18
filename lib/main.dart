e8c68dddf5263145f26cf2cfe131777eb2a7f36d
Abhijeet Vader <a.vader@innopix.solutions>
2019-10-18 12:33:52 +0530

    simple material app commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "My first app",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("My first app appBar"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Hello app"),
+        ),
+      ),
+    ),
+  ));
+}
