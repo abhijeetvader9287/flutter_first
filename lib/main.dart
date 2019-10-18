6a4f4179e1a3b5570cbcc38d0e88e80d69304a5b
Abhijeet Vader <a.vader@innopix.solutions>
2019-10-18 13:02:24 +0530

    custom background color added

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "My first app",
-    home: Scaffold(
+    home: MyStartHome(),
+    /* Scaffold(
       appBar: AppBar(
         title: Text("My first app appBar"),
       ),
@@ -12,11 +13,28 @@ void main() {
           child: Text("Hello app"),
         ),
       ),
-    ),
+    ),*/
     theme: ThemeData(
         primarySwatch: Colors.red,
         accentColor: Colors.greenAccent,
         backgroundColor: Colors.orange
     ),
   ));
+}
+class MyStartHome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Using theme"),
+      ),
+      body: Container(
+        // set background color
+        decoration: BoxDecoration(color: Colors.yellow),
+
+      ),
+    );
+  }
+
 }
