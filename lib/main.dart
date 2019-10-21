49fd7693f853f9c2970a689d64750d623f5b8df7
Abhijeet Vader <a.vader@innopix.solutions>
2019-10-21 13:31:45 +0530

    loading get http data added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 void main() {
   runApp(MaterialApp(
     title: "My first app",
-    home: MyLoadLocalJsonApp(),
+    home: MyGetHttpData(),
     /* Scaffold(
       appBar: AppBar(
         title: Text("My first app appBar"),
@@ -292,8 +294,95 @@ class MyLoadLocalJsonAppState  extends State<MyLoadLocalJsonApp>{
   }
 }
 
+/*********************************************/
+
+class MyGetHttpData extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyGetHttpDataState();
+  }
+
+}
+
+class MyGetHttpDataState extends State<MyGetHttpData> {
+  final String url="https://swapi.co/api/people";
+  List data;
 
 
+  // Function to get the JSON data
+  Future<String> getJSONData() async {
+    var response = await http.get(
+      // Encode the url
+        Uri.encodeFull(url),
+        // Only accept JSON response
+        headers: {"Accept": "application/json"});
+
+    // Logs the response body to the console
+    print(response.body);
+
+    // To modify the state of the app, use this method
+    setState(() {
+      // Get the JSON data
+      var dataConvertedToJSON = json.decode(response.body);
+      // Extract the required part and assign it to the global variable named data
+      data = dataConvertedToJSON['results'];
+    });
+
+    return "Successfull";
+  }
+
+
+
+
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Retrieve JSON Data via HTTP GET"),
+      ),
+      // Create a Listview and load the data when available
+      body: ListView.builder(
+          itemCount: data == null ? 0 : data.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: Center(
+                  child: Column(
+                    // Stretch the cards in horizontal axis
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Card(
+                        child: Container(
+                          child: Text(
+                            // Read the name field value and set it in the Text widget
+                            data[index]['name'],
+                            // set some style to text
+                            style: TextStyle(
+                                fontSize: 20.0, color: Colors.lightBlueAccent),
+                          ),
+                          // added padding
+                          padding: const EdgeInsets.all(15.0),
+                        ),
+                      )
+                    ],
+                  )),
+            );
+          }),
+    );
+  }
+  @override
+  void initState() {
+    super.initState();
+
+    // Call the getJSONData() method when the app initializes
+    this.getJSONData();
+  }
+}
 
 
 
