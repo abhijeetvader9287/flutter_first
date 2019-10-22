0bbe3c6a937798ef757d66bfc22e16759b87637b
Abhijeet Vader <a.vader@innopix.solutions>
2019-10-22 14:31:20 +0530

    bottom navigation view added

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:http/http.dart' as http;
 void main() {
   runApp(MaterialApp(
     title: "My first app",
-    home: MyTabDemo(),
+    home: MyBottomNavTabBar(),
     /* Scaffold(
       appBar: AppBar(
         title: Text("My first app appBar"),
@@ -27,6 +27,82 @@ void main() {
         backgroundColor: Colors.orange),
   ));
 }
+
+
+
+class MyBottomNavTabBar extends StatefulWidget
+{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyBottomNavTabBarState();
+  }
+
+}
+class MyBottomNavTabBarState extends State<MyBottomNavTabBar> with SingleTickerProviderStateMixin
+{
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the Tab Controller
+    controller = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the Tab Controller
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      // Appbar
+      appBar: AppBar(
+        // Title
+        title: Text("Bottom Navigation Bar Demo"),
+        // Set the background color of the App Bar
+        backgroundColor: Colors.blue,
+      ),
+      // Set the TabBar view as the body of the Scaffold
+      body: TabBarView(
+        // Add tabs as widgets
+        children: <Widget>[First(), Second(), Third()],
+        // set the controller
+        controller: controller,
+      ),
+      // Set the bottom navigation bar
+      bottomNavigationBar: Material(
+        // set the color of the bottom navigation bar
+        color: Colors.blue,
+        // set the tab bar as the child of bottom navigation bar
+        child: TabBar(
+          tabs: <Tab>[
+            Tab(
+              // set icon to the tab
+              icon: Icon(Icons.favorite),
+            ),
+            Tab(
+              icon: Icon(Icons.adb),
+            ),
+            Tab(
+              icon: Icon(Icons.airport_shuttle),
+            ),
+          ],
+          // setup the controller
+          controller: controller,
+        ),
+      ),
+    );;
+  }
+
+}
+
 
 class MyTabDemo extends StatefulWidget{
   @override
