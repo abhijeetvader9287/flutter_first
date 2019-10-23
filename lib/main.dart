95e2c28a1d592289a2b7162cfa8f7f2a0cce0b92
Abhijeet Vader <a.vader@innopix.solutions>
2019-10-23 15:05:34 +0530

    shared preferences demo added

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,115 @@ import './utils.dart' as utils;
 import 'package:flutter_app/gridview.dart';
 import 'package:english_words/english_words.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 
+void main() {
+  runApp(MaterialApp(
+    // Disable the debug flag
+      debugShowCheckedModeBanner: false,
+      // Home
+      home: MySharedPreferenceHome ()));
+}
+
+class MySharedPreferenceHome extends StatefulWidget {
+  @override
+  MySharedPreferenceHomeState createState() {
+    return MySharedPreferenceHomeState();
+  }
+}
+
+class MySharedPreferenceHomeState extends State<MySharedPreferenceHome> {
+  var nameOfApp = "Persist Key Value";
+
+  var counter = 0;
+
+  // define a key to use later
+  var key = "counter";
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSavedData();
+  }
+
+  _loadSavedData() async {
+    // Get shared preference instance
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      // Get value
+      counter = (prefs.getInt(key) ?? 0);
+    });
+  }
+
+  _onIncrementHit() async {
+    // Get shared preference instance
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      // Get value
+      counter = (prefs.getInt(key) ?? 0) + 1;
+    });
+
+    // Save Value
+    prefs.setInt(key, counter);
+  }
+
+  _onDecrementHit() async {
+    // Get shared preference instance
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      // Get value
+      counter = (prefs.getInt(key) ?? 0) - 1;
+    });
+
+    // Save Value
+    prefs.setInt(key, counter);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Appbar
+      appBar: AppBar(
+        // Title
+        title: Text(nameOfApp),
+      ),
+      // Body
+      body: Container(
+        // Center the content
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '$counter',
+                textScaleFactor: 10.0,
+              ),
+              Padding(padding: EdgeInsets.all(10.0)),
+              RaisedButton(
+                  onPressed: _onIncrementHit,
+                  child: Text('Increment Counter')),
+              Padding(padding: EdgeInsets.all(10.0)),
+              RaisedButton(
+                  onPressed: _onDecrementHit,
+                  child: Text('Decrement Counter')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+/*
+// infinite List
 void main() => runApp(MyInfiniteListApp());
 
 class MyInfiniteListApp extends StatelessWidget {
@@ -139,7 +245,7 @@ class RandomWordsState extends State<RandomWords> {
           return _buildRow(_suggestions[index]);
         });
   }
-}
+}*/
 
 
 
