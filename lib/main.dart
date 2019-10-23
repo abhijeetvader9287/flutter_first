9cc7346f982ff7620e63b72207b5af31fe8b234e
Abhijeet Vader <a.vader@innopix.solutions>
2019-10-23 12:15:53 +0530

    image from network demo added

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,34 @@ import './utils.dart' as utils;
 
 import 'package:flutter_app/gridview.dart';
 
+void main() => runApp(MyImageFromNetwordApp());
 
+class MyImageFromNetwordApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Image from Network"),
+        ),
+        body: Container(
+            child: Column(
+              children: <Widget>[
+                // Load image from network
+                Image.network(
+                    'https://github.com/nisrulz/flutter-examples/raw/develop/image_from_network/img/flutter_logo.png'),
+                // even loads gifs
+                // Gif image from Giphy, all copyrights are owned by Giphy
+                Image.network(
+                    'https://github.com/nisrulz/flutter-examples/raw/develop/image_from_network/img/loop_anim.gif'),
+              ],
+            )),
+      ),
+    );
+  }
+}
+/*
+// Dropdown list demo
 void main() => runApp(MyDropdownListApp());
 
 class MyDropdownListApp extends StatefulWidget {
@@ -81,6 +108,7 @@ class MyDropdownListAppState extends State<MyDropdownListApp> {
   }
 }
 
+*/
 
 
 
