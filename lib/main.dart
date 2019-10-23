a97bee54d77a0751d68e0227c341aa1a02e4e415
Abhijeet Vader <a.vader@innopix.solutions>
2019-10-23 12:02:14 +0530

    drop down list demo added

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,77 @@ import './utils.dart' as utils;
 
 import 'package:flutter_app/gridview.dart';
 
+
+void main() => runApp(MyDropdownListApp());
+
+class MyDropdownListApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyDropdownListAppState();
+  }
+}
+
+class MyDropdownListAppState extends State<MyDropdownListApp> {
+  List _fruits = ["Apple", "Banana", "Pineapple", "Mango", "Grapes"];
+
+  List<DropdownMenuItem<String>> _dropDownMenuItems;
+  String _selectedFruit;
+
+  @override
+  void initState() {
+    _dropDownMenuItems = buildAndGetDropDownMenuItems(_fruits);
+    _selectedFruit = _dropDownMenuItems[0].value;
+    super.initState();
+  }
+
+  List<DropdownMenuItem<String>> buildAndGetDropDownMenuItems(List fruits) {
+    List<DropdownMenuItem<String>> items = List();
+    for (String fruit in fruits) {
+      items.add(DropdownMenuItem(value: fruit, child: Text(fruit)));
+    }
+    return items;
+  }
+
+  void changedDropDownItem(String selectedFruit) {
+    setState(() {
+      _selectedFruit = selectedFruit;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("DropDown Button Example"),
+        ),
+        body: Container(
+          child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Please choose a fruit: "),
+                  DropdownButton(
+                    value: _selectedFruit,
+                    items: _dropDownMenuItems,
+                    onChanged: changedDropDownItem,
+                  )
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+/*
+//Gridview
 void main() => runApp(MyGridViewApp());
 
 class MyGridViewApp extends StatelessWidget {
@@ -35,7 +106,7 @@ class MyGridViewApp extends StatelessWidget {
       ),
     );
   }
-}
+}*/
 
 /*
 import 'package:splashscreen/splashscreen.dart';
